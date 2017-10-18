3857e0fe576a7bc78d8904e15245493c700a59dd
John Wang <johncwang@gmail.com>
2017-10-17 21:18:38 -0700

    add Salesforce from ENV methods

--- services/salesforce/salesforce.go
+++ services/salesforce/salesforce.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
 
+	"github.com/grokify/gotilla/net/httputilmore"
 	ou "github.com/grokify/oauth2util-go"
 	"golang.org/x/oauth2"
 	"gopkg.in/jeevatkm/go-model.v1"
@@ -35,6 +37,45 @@ func NewClientPassword(app ou.ApplicationCredentials, user ou.UserCredentials) (
 	}
 
 	return ou.NewClientPasswordConf(conf, user.Username, user.Password)
+}
+
+func NewClientPasswordSalesforceEnv() (*http.Client, error) {
+	return NewClientPassword(
+		ou.ApplicationCredentials{
+			ClientID:     os.Getenv("SALESFORCE_CLIENT_ID"),
+			ClientSecret: os.Getenv("SALESFORCE_CLIENT_SECRET")},
+		ou.UserCredentials{
+			Username: os.Getenv("SALESFORCE_USERNAME"),
+			Password: fmt.Sprintf("%v%v",
+				os.Getenv("SALESFORCE_PASSWORD"),
+				os.Getenv("SALESFORCE_SECURITY_KEY"))})
+}
+
+type SalesforceClient struct {
+	ClientMore httputilmore.ClientMore
+	URLBuilder URLBuilder
+}
+
+func NewSalesforceClientEnv() (SalesforceClient, error) {
+	sc := SalesforceClient{
+		URLBuilder: NewURLBuilder(os.Getenv("SALESFORCE_INSTANCE_NAME")),
+	}
+	client, err := NewClientPasswordSalesforceEnv()
+	if err != nil {
+		return sc, err
+	}
+	sc.ClientMore = httputilmore.ClientMore{Client: client}
+	return sc, nil
+}
+
+func (sc *SalesforceClient) GetServicesData() (*http.Response, error) {
+	apiURL := sc.URLBuilder.Build("services/data")
+	return sc.ClientMore.Client.Get(apiURL.String())
+}
+
+func (sc *SalesforceClient) CreateContact(contact interface{}) (*http.Response, error) {
+	apiURL := sc.URLBuilder.Build("/services/data/v40.0/sobjects/Contact/")
+	return sc.ClientMore.PostToJSON(apiURL.String(), contact)
 }
 
 type URLBuilder struct {
