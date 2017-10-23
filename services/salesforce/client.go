c3863908deba7e0cf4fe5855cc7677053523809b
John Wang <johncwang@gmail.com>
2017-10-23 02:53:43 -0700

    update salesforce

---
+++ services/salesforce/client.go
@@ -0,0 +1,50 @@
+package salesforce
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	ou "github.com/grokify/oauth2util-go"
+	"golang.org/x/oauth2"
+	"gopkg.in/jeevatkm/go-model.v1"
+)
+
+const (
+	AuthzURL        = "https://login.salesforce.com/services/oauth2/authorize"
+	TokenURL        = "https://login.salesforce.com/services/oauth2/token"
+	RevokeURL       = "https://login.salesforce.com/services/oauth2/revoke"
+	ServerURLFormat = "https://%v.salesforce.com"
+	HostFormat      = "%v.salesforce.com"
+)
+
+var Endpoint = oauth2.Endpoint{
+	AuthURL:  AuthzURL,
+	TokenURL: TokenURL,
+}
+
+func NewClientPassword(app ou.ApplicationCredentials, user ou.UserCredentials) (*http.Client, error) {
+	conf := oauth2.Config{
+		ClientID:     app.ClientID,
+		ClientSecret: app.ClientSecret}
+
+	if model.IsZero(app.Endpoint) {
+		conf.Endpoint = Endpoint
+	} else {
+		conf.Endpoint = app.Endpoint
+	}
+
+	return ou.NewClientPasswordConf(conf, user.Username, user.Password)
+}
+
+func NewClientPasswordSalesforceEnv() (*http.Client, error) {
+	return NewClientPassword(
+		ou.ApplicationCredentials{
+			ClientID:     os.Getenv("SALESFORCE_CLIENT_ID"),
+			ClientSecret: os.Getenv("SALESFORCE_CLIENT_SECRET")},
+		ou.UserCredentials{
+			Username: os.Getenv("SALESFORCE_USERNAME"),
+			Password: fmt.Sprintf("%v%v",
+				os.Getenv("SALESFORCE_PASSWORD"),
+				os.Getenv("SALESFORCE_SECURITY_KEY"))})
+}
