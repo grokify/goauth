8eac0f353a3139881784852e63b39a75fc3a2cb8
John Wang <johncwang@gmail.com>
2017-11-17 23:45:14 +0800

    add aha.NewClient()

--- aha/aha.go
+++ aha/aha.go
@@ -2,17 +2,33 @@ package aha
 
 import (
 	"fmt"
+	"net/http"
 
+	hum "github.com/grokify/gotilla/net/httputilmore"
+	ou "github.com/grokify/oauth2util"
 	"golang.org/x/oauth2"
 )
 
 const (
-	AuthURLFormat  = "https://%s.aha.io/oauth/authorize"
-	TokenURLFormat = "https://%s.aha.io/oauth/token"
+	AuthURLFormat    = "https://%s.aha.io/oauth/authorize"
+	TokenURLFormat   = "https://%s.aha.io/oauth/token"
+	AhaAccountHeader = "X-AHA-ACCOUNT"
 )
 
 func NewEndpoint(subdomain string) oauth2.Endpoint {
 	return oauth2.Endpoint{
 		AuthURL:  fmt.Sprintf(AuthURLFormat, subdomain),
 		TokenURL: fmt.Sprintf(TokenURLFormat, subdomain)}
+}
+
+func NewClient(subdomain, token string) *http.Client {
+	client := ou.NewClientAccessToken(token)
+
+	header := http.Header{}
+	header.Add(AhaAccountHeader, subdomain)
+
+	client.Transport = hum.TransportWithHeaders{
+		Transport: client.Transport,
+		Header:    header}
+	return client
 }
