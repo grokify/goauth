f128c7415f4462ddbd0d82d0ab831267c6c9880c
John Wang <johncwang@gmail.com>
2018-01-04 08:55:26 -0800

    update ringcentral

--- ringcentral/client.go
+++ ringcentral/client.go
@@ -1,18 +1,21 @@
 package ringcentral
 
 import (
+	"net/http"
+	"os"
+	"strings"
+
 	ou "github.com/grokify/oauth2more"
 	"golang.org/x/oauth2"
-	"net/http"
 )
 
 var (
-	EnvServerURL    = "RC_SERVER_URL"
-	EnvClientID     = "RC_CLIENT_ID"
-	EnvClientSecret = "RC_CLIENT_SECRET"
-	EnvUsername     = "RC_USER_USERNAME"
-	EnvExtension    = "RC_USER_EXTENSION"
-	EnvPassword     = "RC_USER_PASSWORD"
+	EnvServerURL    = "RINGCENTRAL_SERVER_URL"
+	EnvClientID     = "RINGCENTRAL_CLIENT_ID"
+	EnvClientSecret = "RINGCENTRAL_CLIENT_SECRET"
+	EnvUsername     = "RINGCENTRAL_USERNAME"
+	EnvExtension    = "RINGCENTRAL_EXTENSION"
+	EnvPassword     = "RINGCENTRAL_PASSWORD"
 )
 
 type ApplicationCredentials struct {
@@ -36,12 +39,34 @@ type UserCredentials struct {
 	Password  string
 }
 
+func (uc *UserCredentials) UsernameSimple() string {
+	if len(strings.TrimSpace(uc.Extension)) > 0 {
+		return strings.Join([]string{uc.Username, uc.Extension}, "*")
+	}
+	return uc.Username
+}
+
 func NewClientPassword(app ApplicationCredentials, user UserCredentials) (*http.Client, error) {
 	return ou.NewClientPasswordConf(
 		oauth2.Config{
 			ClientID:     app.ClientID,
 			ClientSecret: app.ClientSecret,
 			Endpoint:     NewEndpoint(app.ServerURL)},
-		user.Username,
+		user.UsernameSimple(),
 		user.Password)
+}
+
+func NewClientPasswordEnv() (*http.Client, error) {
+	return NewClientPassword(
+		ApplicationCredentials{
+			ServerURL:    os.Getenv(EnvServerURL),
+			ClientID:     os.Getenv(EnvClientID),
+			ClientSecret: os.Getenv(EnvClientSecret),
+		},
+		UserCredentials{
+			Username:  os.Getenv(EnvUsername),
+			Password:  os.Getenv(EnvPassword),
+			Extension: os.Getenv(EnvExtension),
+		},
+	)
 }
