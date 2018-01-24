3fac7d49ad923d47112f9fff71b1b9a28179d3f3
John Wang <johncwang@gmail.com>
2018-01-23 22:17:59 -0800

    add multiservice/beegouser

--- multiservice/multiservice.go
+++ multiservice/multiservice.go
@@ -1,7 +1,9 @@
 package multiservice
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"regexp"
 	"strings"
 
@@ -15,15 +17,67 @@ import (
 	"github.com/grokify/oauth2more/ringcentral"
 )
 
-type AppConfigs struct {
+type OAuth2Manager struct {
+	ConfigSet *ConfigSet
+	TokenSet  *TokenSet
+}
+
+func NewOAuth2Manager() *OAuth2Manager {
+	return &OAuth2Manager{
+		ConfigSet: NewConfigSet(),
+		TokenSet:  NewTokenSet(),
+	}
+}
+
+func (cb *OAuth2Manager) GetClient(ctx context.Context, serviceKey string) (*http.Client, error) {
+	if cb.ConfigSet == nil {
+		return nil, fmt.Errorf("OAuth2Manager.ConfigSet == nil")
+	}
+	if cb.TokenSet == nil {
+		return nil, fmt.Errorf("OAuth2Manager.TokenSet == nil")
+	}
+
+	cfg, err := cb.ConfigSet.Get(serviceKey)
+	if err != nil {
+		return nil, err
+	}
+	tok, err := cb.TokenSet.Get(serviceKey)
+	if err != nil {
+		return nil, err
+	}
+	return cfg.Client(ctx, tok), nil
+}
+
+type TokenSet struct {
+	TokenMap map[string]*TokenInfo
+}
+
+func NewTokenSet() *TokenSet {
+	return &TokenSet{TokenMap: map[string]*TokenInfo{}}
+}
+
+func (toks *TokenSet) Get(key string) (*oauth2.Token, error) {
+	if tok, ok := toks.TokenMap[key]; ok {
+		return tok.Token, nil
+	}
+	return nil, fmt.Errorf("AppConfig not found for %v", key)
+}
+
+type TokenInfo struct {
+	ServiceKey  string
+	ServiceType string
+	Token       *oauth2.Token
+}
+
+type ConfigSet struct {
 	ConfigsMap map[string]*oauth2.Config
 }
 
-func NewAppConfigs() *AppConfigs {
-	return &AppConfigs{ConfigsMap: map[string]*oauth2.Config{}}
+func NewConfigSet() *ConfigSet {
+	return &ConfigSet{ConfigsMap: map[string]*oauth2.Config{}}
 }
 
-func (cfgs *AppConfigs) AddAppConfigWrapperBytes(key string, val []byte) error {
+func (cfgs *ConfigSet) AddAppConfigWrapperBytes(key string, val []byte) error {
 	acw, err := oauth2more.NewAppCredentialsWrapperFromBytes(val)
 	if err != nil {
 		return err
@@ -31,7 +85,7 @@ func (cfgs *AppConfigs) AddAppConfigWrapperBytes(key string, val []byte) error {
 	return cfgs.AddAppConfigWrapper(key, acw)
 }
 
-func (cfgs *AppConfigs) AddAppConfigWrapper(key string, acw oauth2more.AppCredentialsWrapper) error {
+func (cfgs *ConfigSet) AddAppConfigWrapper(key string, acw oauth2more.AppCredentialsWrapper) error {
 	cfg, err := acw.Config()
 	if err != nil {
 		return err
@@ -40,14 +94,21 @@ func (cfgs *AppConfigs) AddAppConfigWrapper(key string, acw oauth2more.AppCreden
 	return nil
 }
 
-func (cfgs *AppConfigs) Get(key string) (*oauth2.Config, error) {
+func (cfgs *ConfigSet) Has(key string) bool {
+	if _, ok := cfgs.ConfigsMap[key]; ok {
+		return true
+	}
+	return false
+}
+
+func (cfgs *ConfigSet) Get(key string) (*oauth2.Config, error) {
 	if cfg, ok := cfgs.ConfigsMap[key]; ok {
 		return cfg, nil
 	}
 	return nil, fmt.Errorf("AppConfig not found for %v", key)
 }
 
-func (cfgs *AppConfigs) MustGet(key string) *oauth2.Config {
+func (cfgs *ConfigSet) MustGet(key string) *oauth2.Config {
 	c, err := cfgs.Get(key)
 	if err != nil {
 		panic(err)
@@ -55,10 +116,35 @@ func (cfgs *AppConfigs) MustGet(key string) *oauth2.Config {
 	return c
 }
 
+func (cfgs *ConfigSet) Slugs() []string {
+	slugs := []string{}
+	for slug, _ := range cfgs.ConfigsMap {
+		slugs = append(slugs, slug)
+	}
+	return slugs
+}
+
+func (cfgs *ConfigSet) ClientURLsMap() map[string]AppURLs {
+	apps := map[string]AppURLs{}
+	for slug, cfg := range cfgs.ConfigsMap {
+		apps[slug] = AppURLs{
+			AuthURL:     cfg.Endpoint.AuthURL,
+			RedirectURL: cfg.RedirectURL,
+		}
+	}
+	return apps
+}
+
+type AppURLs struct {
+	AuthURL     string `json:"authUrl,omitempty"`
+	TokenURL    string `json:"tokenUrl,omitempty"`
+	RedirectURL string `json:"redirectUrl,omitempty"`
+}
+
 // EnvOAuth2ConfigMap returns a map of *oauth2.Config from environment
 // variables in AppCredentialsWrapper format.
-func EnvOAuth2ConfigMap(env []osutil.EnvVar, prefix string) (*AppConfigs, error) {
-	cfgs := NewAppConfigs()
+func EnvOAuth2ConfigMap(env []osutil.EnvVar, prefix string) (*ConfigSet, error) {
+	cfgs := NewConfigSet()
 
 	rx, err := regexp.Compile(fmt.Sprintf(`^%v(.*)`, prefix))
 	if err != nil {
