2f4de3f141825141291cd110e4e7f5b63ee1a60a
John Wang <johncwang@gmail.com>
2018-01-29 04:57:39 -0800

    rename files

---
+++ aha/aha_client.go
@@ -0,0 +1,40 @@
+package aha
+
+import (
+	"fmt"
+	"net/http"
+
+	hum "github.com/grokify/gotilla/net/httputilmore"
+	ou "github.com/grokify/oauth2more"
+	"golang.org/x/oauth2"
+)
+
+const (
+	APIMeURL         = "https://secure.aha.io/api/v1/me"
+	AuthURLFormat    = "https://%s.aha.io/oauth/authorize"
+	TokenURLFormat   = "https://%s.aha.io/oauth/token"
+	AhaAccountHeader = "X-AHA-ACCOUNT"
+)
+
+var (
+	AhaAccountEnv = "AHA_ACCOUNT"
+	AhaApiKeyEnv  = "AHA_API_KEY"
+)
+
+func NewEndpoint(subdomain string) oauth2.Endpoint {
+	return oauth2.Endpoint{
+		AuthURL:  fmt.Sprintf(AuthURLFormat, subdomain),
+		TokenURL: fmt.Sprintf(TokenURLFormat, subdomain)}
+}
+
+func NewClient(subdomain, token string) *http.Client {
+	client := ou.NewClientAccessToken(token)
+
+	header := http.Header{}
+	header.Add(AhaAccountHeader, subdomain)
+
+	client.Transport = hum.TransportWithHeaders{
+		Transport: client.Transport,
+		Header:    header}
+	return client
+}
