b0fa73e83d428431c111a6d695884f8cbcd3e44c
John Wang <johncwang@gmail.com>
2020-08-02 01:58:03 -0700

    feat: add SCIM user address, IETF RFC-7643

--- scim/definitions.go
+++ scim/definitions.go
@@ -11,22 +11,23 @@ import (
 // specified in the SCIM schema:
 // http://www.simplecloud.info/specs/draft-scim-core-schema-01.html#anchor7
 type User struct {
-	Schemas           []string `json:"schemas,omitempty"`
-	ExternalId        string   `json:"externalId,omitempty"`
-	UserName          string   `json:"userName,omitempty"`
-	Name              Name     `json:"name,omitempty"`
-	DisplayName       string   `json:"displayName,omitempty"`
-	NickName          string   `json:"nickName,omitempty"`
-	ProfileUrl        string   `json:"profileUrl,omitempty"`
-	PhoneNumbers      []Item   `json:"phoneNumbers,omitempty"`
-	Emails            []Item   `json:"emails,omitempty"`
-	UserType          string   `json:"userType,omitempty"`
-	Title             string   `json:"title,omitempty"`
-	PreferredLanguage string   `json:"preferredLanguage,omitempty"`
-	Locale            string   `json:"locale,omitempty"`
-	Timezone          string   `json:"timezone,omitempty"`
-	Active            bool     `json:"active,omitempty"`
-	Password          string   `json:"password,omitempty"`
+	Schemas           []string  `json:"schemas,omitempty"`
+	ExternalId        string    `json:"externalId,omitempty"`
+	UserName          string    `json:"userName,omitempty"`
+	Name              Name      `json:"name,omitempty"`
+	DisplayName       string    `json:"displayName,omitempty"`
+	NickName          string    `json:"nickName,omitempty"`
+	ProfileUrl        string    `json:"profileUrl,omitempty"`
+	PhoneNumbers      []Item    `json:"phoneNumbers,omitempty"`
+	Emails            []Item    `json:"emails,omitempty"`
+	UserType          string    `json:"userType,omitempty"`
+	Title             string    `json:"title,omitempty"`
+	PreferredLanguage string    `json:"preferredLanguage,omitempty"`
+	Locale            string    `json:"locale,omitempty"`
+	Timezone          string    `json:"timezone,omitempty"`
+	Active            bool      `json:"active,omitempty"`
+	Password          string    `json:"password,omitempty"`
+	Addresses         []Address `json:"addresses,omitempty"`
 }
 
 func (user *User) DisplayNameAny() string {
@@ -98,4 +99,16 @@ type Item struct {
 	Value   string `json:"value,omitempty"`
 	Type    string `json:"type,omitempty"`
 	Primary bool   `json:"primary,omitempty"`
+}
+
+// Address is a SCIM struct used for street and mailing addresses.
+type Address struct {
+	Type          string `json:"Type,omitempty"`
+	StreetAddress string `json:"StreetAddress,omitempty"`
+	Locality      string `json:"Locality,omitempty"`
+	Region        string `json:"Region,omitempty"`
+	PostalCode    string `json:"postalCode,omitempty"`
+	Country       string `json:"country,omitempty"`
+	Formatted     string `json:"formatted,omitempty"`
+	Primary       bool   `json:"primary,omitempty"`
 }
