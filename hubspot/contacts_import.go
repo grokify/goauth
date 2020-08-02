7a37b716387cd3bc5341f90fab4b5fe138d57ea0
John Wang <johncwang@gmail.com>
2020-08-02 05:24:46 -0700

    feat: hubspot.WriteContactsXLSX()

---
+++ hubspot/contacts_import.go
@@ -0,0 +1,58 @@
+package hubspot
+
+import (
+	"strings"
+
+	"github.com/grokify/gocharts/data/table"
+	"github.com/grokify/gotilla/strconv/phonenumber"
+	"github.com/grokify/gotilla/strings/usstate"
+	"github.com/grokify/oauth2more/scim"
+	"github.com/nyaruka/phonenumbers"
+)
+
+func WriteContactsXLSX(filename string, users []scim.User) error {
+	sheetdata := table.SheetData{
+		SheetName: "Contacts",
+		Rows: [][]interface{}{
+			columnsInterface()}}
+
+	for _, user := range users {
+		row := userToScim(user)
+		sheetdata.Rows = append(sheetdata.Rows, row)
+	}
+	return table.WriteXLSXInterface(filename, sheetdata)
+}
+
+func userToScim(user scim.User) []interface{} {
+	row := []interface{}{
+		user.Name.GivenName,
+		user.Name.FamilyName,
+		user.EmailAddress(),
+		phonenumber.MustE164Format(user.PhoneNumber(), "US", phonenumbers.NATIONAL)}
+	if len(user.Addresses) > 0 {
+		addr := user.Addresses[0]
+		row = append(row,
+			addr.StreetAddress,
+			addr.Locality,
+			usstate.Abbreviate(addr.Region),
+			addr.PostalCode)
+	} else {
+		row = append(row, "", "", "", "")
+	}
+	return row
+}
+
+const ColumnsString = `First Name,Last Name,Email Address,Phone Number,Street Address,City,State,Postal Code`
+
+func Columns() []string {
+	return strings.Split(ColumnsString, ",")
+}
+
+func columnsInterface() []interface{} {
+	cols := []interface{}{}
+	strs := Columns()
+	for _, str := range strs {
+		cols = append(cols, str)
+	}
+	return cols
+}
