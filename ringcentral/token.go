5fb2d975c58a0ce9f4c7073761a7717080756da1
John Wang <johncwang@gmail.com>
2021-01-11 20:53:56 -0800

    feat: ringcentral: add NewTokenCli()

---
+++ ringcentral/token.go
@@ -0,0 +1,35 @@
+package ringcentral
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/grokify/oauth2more"
+	"golang.org/x/oauth2"
+)
+
+func NewTokenCli(creds Credentials, state string) (*oauth2.Token, error) {
+	var token *oauth2.Token
+	var err error
+
+	if creds.Application.IsGrantType(oauth2more.GrantTypeAuthorizationCode) {
+		state = strings.TrimSpace(state)
+		if len(state) == 0 {
+			state = "oauth2more-" + time.Now().UTC().Format(time.RFC3339)
+		}
+		fmt.Printf("OAuth State [%s]\n", state)
+		cfg := creds.Application.Config()
+		token, err = oauth2more.NewTokenFromWeb(&cfg, state)
+		if err != nil {
+			return token, err
+		}
+	} else {
+		token, err = creds.NewToken()
+		if err != nil {
+			return token, err
+		}
+	}
+	token.Expiry = token.Expiry.UTC()
+	return token, nil
+}
