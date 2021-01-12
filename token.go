aaab99361a7b7246ee95a5de2ce68b20b39a8d9c
John Wang <johncwang@gmail.com>
2021-01-11 20:50:47 -0800

    move: oauth2more: move func

--- token.go
+++ token.go
@@ -2,11 +2,13 @@ package oauth2more
 
 import (
 	"encoding/json"
+	"fmt"
 
+	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 )
 
-// ParseToken parses a JSON token and returns an
+// ParseToken parses a OAuth 2 token and returns an
 // `*oauth2.Token` with custom properties.
 func ParseToken(rawToken []byte) (*oauth2.Token, error) {
 	tok := &oauth2.Token{}
@@ -17,7 +19,23 @@ func ParseToken(rawToken []byte) (*oauth2.Token, error) {
 	msi := map[string]interface{}{}
 	err = json.Unmarshal(rawToken, &msi)
 	if err != nil {
-		return nil, err
+		return tok, err
 	}
 	return tok.WithExtra(msi), nil
+}
+
+func NewTokenFromWeb(cfg *oauth2.Config, state string) (*oauth2.Token, error) {
+	authURL := cfg.AuthCodeURL(state, oauth2.AccessTypeOffline)
+	fmt.Printf("Go to this link in your browser then type in the auth code from the webpage and click `return` to continue: \n%v\n", authURL)
+
+	code := ""
+	if _, err := fmt.Scan(&code); err != nil {
+		return nil, errors.Wrap(err, "Unable to read auth code")
+	}
+
+	tok, err := cfg.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return tok, errors.Wrap(err, "Unable to retrieve token from web")
+	}
+	return tok, nil
 }
