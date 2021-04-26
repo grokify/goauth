90bbcef251179b518e47a1f0550dfff71ab9b998
John Wang <johncwang@gmail.com>
2021-04-26 01:33:11 -0700

    feat: zoom: add `NewClient()`

--- zoom/zoom_jwt.go
+++ zoom/zoom_jwt.go
@@ -15,14 +15,22 @@ const (
 	HeaderUserAgentJwtValue = "Zoom-api-Jwt-Request"
 )
 
-func CreateJwtToken(apiKey, apiSecret string, tokenDur time.Duration) (*jwt.Token, string, error) {
+func CreateJwtToken(apiKey, apiSecret string, tokenDuration time.Duration) (*jwt.Token, string, error) {
 	token := jwt.NewWithClaims(
 		jwt.SigningMethodHS256,
 		jwt.StandardClaims{
 			Issuer:    apiKey,
-			ExpiresAt: time.Now().Add(tokenDur).Unix()})
+			ExpiresAt: time.Now().Add(tokenDuration).Unix()})
 	tokenString, err := token.SignedString([]byte(apiSecret))
 	return token, tokenString, err
+}
+
+func NewClient(apiKey, apiSecret string, tokenDuration time.Duration) (*http.Client, error) {
+	_, jwtString, err := CreateJwtToken(apiKey, apiSecret, tokenDuration)
+	if err != nil {
+		return nil, err
+	}
+	return NewClientToken(jwtString), nil
 }
 
 func NewClientToken(bearerToken string) *http.Client {
