456fdd8b676de5f1cb9cac095be31b2c417b5b10
John Wang <johncwang@gmail.com>
2021-07-30 01:23:54 -0700

    feat: ringcentral: add endpoints

---
+++ ringcentral/endpoints.go
@@ -0,0 +1,8 @@
+package ringcentral
+
+const (
+	AuthURL         = "https://platform.ringcentral.com/restapi/oauth/authorize"
+	TokenURL        = "https://platform.ringcentral.com/restapi/oauth/token"
+	AuthURLSandbox  = "https://platform.devtest.ringcentral.com/restapi/oauth/authorize"
+	TokenURLSandbox = "https://platform.devtest.ringcentral.com/restapi/oauth/token"
+)
