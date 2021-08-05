33d3bfb362728c7c03a38ac5445b09cb5a791b3e
John Wang <johncwang@gmail.com>
2021-08-04 21:42:04 -0700

    feat: credentials: add service name and OAuth endpoint inflation

---
+++ endpoints/data.go
@@ -0,0 +1,8 @@
+package endpoints
+
+const (
+	RingCentralAuthURL         = "https://platform.ringcentral.com/restapi/oauth/authorize"
+	RingCentralTokenURL        = "https://platform.ringcentral.com/restapi/oauth/token"
+	RingCentralAuthURLSandbox  = "https://platform.devtest.ringcentral.com/restapi/oauth/authorize"
+	RingCentralTokenURLSandbox = "https://platform.devtest.ringcentral.com/restapi/oauth/token"
+)
