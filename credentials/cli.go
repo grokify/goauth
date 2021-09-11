eb4a2e1a8de0a4247eb8dca16f289d0a0c6c836d
John Wang <johncwang@gmail.com>
2021-09-11 10:02:49 -0700

    feat: credentials: add embeddable `Options`

---
+++ credentials/cli.go
@@ -0,0 +1,9 @@
+package credentials
+
+// Options is a struct to be used with `github.com/jessevdk/go-flags`.
+// It can be embedded in another struct.
+type Options struct {
+	CredsPath string `long:"creds" description:"Environment File Path" required:"true"`
+	Account   string `long:"account" description:"Environment Variable Name"`
+	CLI       []bool `long:"cli" description:"CLI"`
+}
