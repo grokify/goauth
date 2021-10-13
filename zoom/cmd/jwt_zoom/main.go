535f3eb2a4e122f589a13992b00a0074efb3291a
John Wang <johncwang@gmail.com>
2021-10-13 04:14:53 -0700

    move: `cmd`: move from `examples

---
+++ zoom/cmd/jwt_zoom/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/grokify/oauth2more"
+	"github.com/grokify/oauth2more/zoom"
+	"github.com/grokify/simplego/config"
+	"github.com/grokify/simplego/fmt/fmtutil"
+	"github.com/grokify/simplego/net/http/httpsimple"
+	"github.com/grokify/simplego/net/urlutil"
+)
+
+func main() {
+	files, err := config.LoadDotEnv(
+		".env", os.Getenv("ENV_PATH"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmtutil.PrintJSON(files)
+
+	apiKey := os.Getenv(zoom.EnvZoomApiKey)
+	apiSecret := os.Getenv(zoom.EnvZoomApiSecret)
+
+	tokenString := ""
+	if 1 == 1 {
+		_, tokenString, err = zoom.CreateJwtToken(apiKey, apiSecret, time.Hour)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		tokenString = "tmpToken"
+	}
+
+	fmt.Printf("TOK [%v]\n", tokenString)
+
+	if 1 == 0 {
+		token, err := oauth2more.ParseJwtTokenString(
+			tokenString, apiSecret,
+			&jwt.StandardClaims{Issuer: apiKey})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmtutil.PrintJSON(token.Claims)
+	}
+
+	client := zoom.NewClientToken(tokenString)
+
+	resp, err := client.Get("https://api.zoom.us/v2/users/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("RESP: %s\n", string(bytes))
+
+	cu := zoom.NewClientUtil(client)
+	scimUser, err := cu.GetSCIMUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmtutil.PrintJSON(cu.UserNative)
+	fmtutil.PrintJSON(scimUser)
+
+	resp, err = createMeeting(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytes, err = io.ReadAll(resp.Body)
+	fmt.Println(string(bytes))
+
+	fmt.Println("DONE")
+}
+
+func createMeeting(client *http.Client) (*http.Response, error) {
+	sc := httpsimple.SimpleClient{
+		BaseURL:    zoom.ZoomAPIBaseURL,
+		HTTPClient: client}
+	req := httpsimple.SimpleRequest{
+		Method: http.MethodPost,
+		URL:    urlutil.JoinAbsolute(zoom.ZoomAPIMeURL, "meetings"),
+		Body:   []byte(reqBody),
+		IsJSON: true}
+	return sc.Do(req)
+}
+
+const reqBody = `{
+	"topic":"MeetingOne",
+	"type":2,
+	"start_time":"2021-07-04T00:00:00Z",
+	"duration":30,
+	"agenda":"meet and greet"
+}`
