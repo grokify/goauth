7ec89ae35538d8cde01e6e29fe65bb73bed5a755
John Wang <johncwang@gmail.com>
2021-10-13 17:43:34 -0700

    feat: credentials: add OAuth 2.0 `client_credentials` grant

--- credentials/credentials_application.go
+++ credentials/credentials_application.go
@@ -3,12 +3,22 @@ package credentials
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/grokify/simplego/net/urlutil"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2/clientcredentials"
 
 	"golang.org/x/oauth2"
+)
+
+const (
+	GrantTypeAuthCode          = "authorization_code"
+	GrantTypePassword          = "password"
+	GrantTypeClientCredentials = "client_credentials"
+	GrantTypeRefreshToken      = "refresh_token"
 )
 
 type ApplicationCredentials struct {
@@ -24,6 +34,7 @@ type ApplicationCredentials struct {
 	AccessTokenTTL  int64           `json:"accessTokenTTL,omitempty"`
 	RefreshTokenTTL int64           `json:"refreshTokenTTL,omitempty"`
 	GrantType       string          `json:"grantType,omitempty"`
+	Scopes          []string        `json:"scopes,omitempty"`
 }
 
 func (app *ApplicationCredentials) Config() oauth2.Config {
@@ -59,22 +70,6 @@ func (app *ApplicationCredentials) Exchange(code string) (*oauth2.Token, error) 
 	}
 
 	return cfg.Exchange(context.Background(), code)
-	/*
-		params := url.Values{}
-		params.Set("grant_type", "authorization_code")
-		params.Set("code", code)
-		params.Set("redirect_uri", app.RedirectURL)
-		if len(app.OAuthEndpointID) > 0 {
-			params.Set("endpoint_id", app.OAuthEndpointID)
-		}
-		if app.AccessTokenTTL > 0 {
-			params.Set("accessTokenTtl", strconv.Itoa(int(app.AccessTokenTTL)))
-		}
-		if app.RefreshTokenTTL > 0 {
-			params.Set("refreshTokenTtl", strconv.Itoa(int(app.RefreshTokenTTL)))
-		}
-		return RetrieveRcToken(app.Config(), params)
-	*/
 }
 
 func (ac *ApplicationCredentials) AppNameAndVersion() string {
@@ -99,4 +94,20 @@ func (app *ApplicationCredentials) IsGrantType(grantType string) bool {
 
 func (app *ApplicationCredentials) InflateURL(apiUrlPath string) string {
 	return urlutil.JoinAbsolute(app.ServerURL, apiUrlPath)
+}
+
+// Client returns a `*http.Client` for applications using `client_credentials`
+// grant. The client can be modified using context, e.g. ignoring bad certs or otherwise.
+func (app *ApplicationCredentials) Client(ctx context.Context) (*http.Client, error) {
+	if app.GrantType != GrantTypeClientCredentials {
+		return nil, errors.New("grant type is not client_credentials")
+	}
+
+	config := clientcredentials.Config{
+		ClientID:     app.ClientID,
+		ClientSecret: app.ClientSecret,
+		TokenURL:     app.OAuth2Endpoint.TokenURL,
+		Scopes:       app.Scopes}
+
+	return config.Client(ctx), nil
 }
