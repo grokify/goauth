f1e21cff92c41f3419556fb237302b508051bea3
John Wang <johncwang@gmail.com>
2021-10-15 02:12:41 -0700

    move: oauth2more: OAuth 2.0 grant type constants

---
+++ constants.go
@@ -0,0 +1,8 @@
+package oauth2more
+
+const (
+	GrantTypeAuthCode          = "authorization_code"
+	GrantTypePassword          = "password"
+	GrantTypeClientCredentials = "client_credentials"
+	GrantTypeRefreshToken      = "refresh_token"
+)
