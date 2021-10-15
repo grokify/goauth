25d2d6671623c587df0d362f8a1c68e111408dac
John Wang <johncwang@gmail.com>
2021-10-15 02:13:07 -0700

    feat: oauth2more: add `TokenClientCredentials()`

--- token.go
+++ token.go
@@ -3,9 +3,15 @@ package oauth2more
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
 
+	"github.com/grokify/simplego/net/httputilmore"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 // ParseToken parses a OAuth 2 token and returns an
@@ -43,4 +49,41 @@ func NewTokenCliFromWeb(cfg *oauth2.Config, state string) (*oauth2.Token, error)
 		return tok, errors.Wrap(err, "Unable to retrieve token from web")
 	}
 	return tok, nil
+}
+
+// TokenClientCredentials is an alternative to `clientcredentials.Config.Token()`
+// which does not work for some APIs. More investigation is needed but it appears
+// the issue is encoding the HTTP request body. The approach here uses `&` in the
+// URL encoded values.
+func TokenClientCredentials(cfg clientcredentials.Config) (*oauth2.Token, error) {
+	body := url.Values{}
+	body.Add("grant_type", GrantTypeClientCredentials)
+	for _, scope := range cfg.Scopes {
+		body.Add("scope", scope)
+	}
+	req, err := http.NewRequest(
+		http.MethodPost,
+		cfg.TokenURL,
+		strings.NewReader(body.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	b64, err := RFC7617UserPass(cfg.ClientID, cfg.ClientSecret)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add(httputilmore.HeaderContentType, httputilmore.ContentTypeAppFormUrlEncoded)
+	req.Header.Add(httputilmore.HeaderAuthorization, "Basic "+b64)
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	tok := &oauth2.Token{}
+	return tok, json.Unmarshal(data, tok)
 }
