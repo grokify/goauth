5dd7266582a28391588ccf05c194320bdb5cbe1f
John Wang <johncwang@gmail.com>
2022-02-04 07:50:09 -0800

    feat: add RFC-7523, JWT for OAuth2

--- jwt.go
+++ jwt.go
@@ -1,8 +1,14 @@
 package goauth
 
 import (
+	"net/http"
+	"net/url"
+	"strings"
+
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/grokify/mogo/errors/errorsutil"
+	"github.com/grokify/mogo/net/httputilmore"
+	"golang.org/x/oauth2"
 )
 
 func ParseJwtTokenString(tokenString string, secretKey string, claims jwt.Claims) (*jwt.Token, error) {
@@ -25,4 +31,34 @@ func ParseJwtTokenString(tokenString string, secretKey string, claims jwt.Claims
 		return nil, errorsutil.Wrap(err, "ParseTokenString.jwt.ParseWithClaims")
 	}
 	return token, nil
+}
+
+func NewTokenOAuth2Jwt(tokenURL, clientId, clientSecret, jwtBase64Enc string) (*oauth2.Token, error) {
+	body := url.Values{
+		ParamGrantType: {GrantTypeJWTBearer},
+		ParamAssertion: {jwtBase64Enc}}
+
+	req, err := http.NewRequest(
+		http.MethodPost, tokenURL,
+		strings.NewReader(body.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add(httputilmore.HeaderContentType,
+		httputilmore.ContentTypeAppFormUrlEncoded)
+
+	if len(clientId) > 0 || len(clientSecret) > 0 {
+		b64Enc, err := RFC7617UserPass(clientId, clientSecret)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Add(httputilmore.HeaderAuthorization, TokenBasic+" "+b64Enc)
+	}
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return ParseTokenReader(resp.Body)
 }
