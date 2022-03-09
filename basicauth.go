fab3206e4f0de635997ea9009cf4a495f89ffb52
John Wang <johncwang@gmail.com>
2022-03-08 18:47:31 -0800

    lint: golangci: fix

--- basicauth.go
+++ basicauth.go
@@ -15,7 +15,7 @@ import (
 // RFC7617UserPass base64 encodes a user-id and password per:
 // https://tools.ietf.org/html/rfc7617#section-2
 func RFC7617UserPass(userid, password string) (string, error) {
-	if strings.Index(userid, ":") > -1 {
+	if strings.Contains(userid, ":") {
 		return "", fmt.Errorf(
 			"RFC7617 user-id cannot include a colon (':') [%v]", userid)
 	}
