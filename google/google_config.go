4727c95b6f2249e7015eedded8ce09298613d389
John Wang <johncwang@gmail.com>
2022-03-08 19:45:44 -0800

    lint: golangci: fix

--- google/google_config.go
+++ google/google_config.go
@@ -17,7 +17,7 @@ func ConfigFromFile(file string, scopes []string) (*oauth2.Config, error) {
 	b, err := os.ReadFile(file) // Google client_secret.json
 	if err != nil {
 		return &oauth2.Config{},
-			errorsutil.Wrap(err, fmt.Sprintf("Unable to read client secret file: %v", err))
+			errorsutil.Wrap(err, fmt.Sprintf("unable to read client secret file [%v]", err))
 	}
 	return o2g.ConfigFromJSON(b, scopes...)
 }
@@ -36,21 +36,21 @@ func ConfigFromEnv(envVar string, scopes []string) (*oauth2.Config, error) {
 
 // ConfigFromBytes returns an *oauth2.Config given a byte array
 // containing the Google client_secret.json data.
-func ConfigFromBytes(configJson []byte, scopes []string) (*oauth2.Config, error) {
-	if len(strings.TrimSpace(string(configJson))) == 0 {
-		return nil, errorsutil.Wrap(errors.New("No Credentials Provided"), "goauth/google.ConfigFromBytes()")
+func ConfigFromBytes(configJSON []byte, scopes []string) (*oauth2.Config, error) {
+	if len(strings.TrimSpace(string(configJSON))) == 0 {
+		return nil, errorsutil.Wrap(errors.New("no credentials provided"), "goauth/google.ConfigFromBytes()")
 	}
 
 	if len(scopes) == 0 {
 		cc := CredentialsContainer{}
-		err := json.Unmarshal(configJson, &cc)
+		err := json.Unmarshal(configJSON, &cc)
 		if err != nil {
-			return nil, errorsutil.Wrap(err, "ConfigFromBytes")
+			return nil, errorsutil.Wrap(err, "func ConfigFromBytes")
 		}
 		if len(cc.Scopes) > 0 {
 			scopes = append(scopes, cc.Scopes...)
 		}
 	}
 
-	return o2g.ConfigFromJSON(configJson, scopes...)
+	return o2g.ConfigFromJSON(configJSON, scopes...)
 }
