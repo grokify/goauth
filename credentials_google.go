e0c0f724320715fd342464b15dd2ca18a8498181
John Wang <johncwang@gmail.com>
2023-08-26 22:18:18 -0700

    feat: `Credentials`: add support for GCP Service Accounts

---
+++ credentials_google.go
@@ -0,0 +1,31 @@
+package goauth
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/grokify/goauth/google"
+)
+
+// CredentialsOAuth2 supports OAuth 2.0 authorization_code, password, and client_credentials grant flows.
+type CredentialsGCP struct {
+	GCPCredentials google.Credentials `json:"gcpCredentials,omitempty"`
+	Scopes         []string           `json:"scopes,omitempty"`
+}
+
+// NewClient returns a `*http.Client` and `error`.
+func (cg *CredentialsGCP) NewClient(ctx context.Context) (*http.Client, error) {
+	hclient, err := cg.GCPCredentials.NewClient(ctx, cg.Scopes)
+	return hclient, err
+}
+
+func CredentialsGCPReadFile(name string) (*CredentialsGCP, error) {
+	b, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	var c *CredentialsGCP
+	return c, json.Unmarshal(b, c)
+}
