cc5caa96622cd12b801212b96e0c70765ddc74bc
grokify <johncwang@gmail.com>
2024-05-12 03:46:08 -0700

    enhance: `aha`: update SCIMM user examples

--- aha/examples/scim_user_credentials/main.go
+++ aha/examples/scim_user_credentials/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 
+	"github.com/grokify/goauth"
 	"github.com/grokify/goauth/aha"
+	"github.com/grokify/goauth/endpoints"
 	"github.com/grokify/mogo/fmt/fmtutil"
 	"github.com/grokify/mogo/log/logutil"
+	"golang.org/x/oauth2"
 )
 
 func loadEnv() error {
@@ -24,10 +28,18 @@ func main() {
 		panic(err)
 	}
 
-	sclient, err := aha.NewSimpleClient(
-		os.Getenv(aha.AhaAccountEnv),
-		os.Getenv(aha.AhaAPIKeyEnv),
-	)
+	creds := goauth.Credentials{
+		Type:    goauth.TypeOAuth2,
+		Service: endpoints.ServiceAha,
+		OAuth2: &goauth.CredentialsOAuth2{
+			ServerURL: os.Getenv(aha.AhaServerURL),
+			Token: &oauth2.Token{
+				AccessToken: os.Getenv(aha.AhaAPIKeyEnv),
+			},
+		},
+	}
+
+	sclient, err := creds.NewSimpleClient(context.Background())
 	logutil.FatalErr(err)
 
 	clientUtil := aha.NewClientUtil(nil)
