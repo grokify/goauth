3afed8ca9dbebc25e6ca985ae2d47fe4f94c0a9d
grokify <johncwang@gmail.com>
2024-05-12 03:44:56 -0700

    feat: `aha`: support sub-domain API URLs. Add `NewSimpleClient()`, `BuildCredentials()`, `ClientUtil.SetSimpleClient()`

--- aha/aha_client.go
+++ aha/aha_client.go
@@ -1,17 +1,22 @@
 package aha
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"net/url"
 
-	"github.com/grokify/goauth/authutil"
-	"github.com/grokify/mogo/net/http/httputilmore"
+	"github.com/grokify/goauth"
+	"github.com/grokify/goauth/endpoints"
+	"github.com/grokify/mogo/net/http/httpsimple"
+	"github.com/grokify/mogo/net/urlutil"
 	"golang.org/x/oauth2"
 )
 
 const (
-	APIMeURL         = "https://secure.aha.io/api/v1/me"
+	APIMeURL         = "https://secure.aha.io/api/v1/me" // no longer supported by Aha. Must use sub-domain
+	BaseURLFormat    = "https://%s.aha.io/"
+	APIMeURLFormat   = "https://%s.aha.io/api/v1/me"
+	APIMeURLPath     = "/api/v1/me"
 	AuthURLFormat    = "https://%s.aha.io/oauth/authorize"
 	TokenURLFormat   = "https://%s.aha.io/oauth/token" // #nosec G101
 	AhaAccountHeader = "X-AHA-ACCOUNT"
@@ -19,6 +24,7 @@ const (
 
 var (
 	AhaAccountEnv = "AHA_ACCOUNT"
+	AhaServerURL  = "AHA_SERVER_URL"
 	AhaAPIKeyEnv  = "AHA_API_KEY" // #nosec G101
 )
 
@@ -28,13 +34,47 @@ func NewEndpoint(subdomain string) oauth2.Endpoint {
 		TokenURL: fmt.Sprintf(TokenURLFormat, subdomain)}
 }
 
-func NewClient(subdomain, token string) *http.Client {
-	return authutil.NewClientHeaderQuery(
-		http.Header{
-			httputilmore.HeaderAuthorization: []string{authutil.TokenBearer + " " + token},
-			AhaAccountHeader:                 []string{subdomain}},
-		url.Values{},
-		false)
+func NewClient(subdomain, token string) (*http.Client, error) {
+	creds := BuildCredentials(subdomain, token)
+	client, _, err := creds.OAuth2.NewClient(context.Background())
+	return client, err
+	/*
+		return authutil.NewClientHeaderQuery(
+			http.Header{
+				httputilmore.HeaderAuthorization: []string{authutil.TokenBearer + " " + token},
+				AhaAccountHeader:                 []string{subdomain}},
+			url.Values{},
+			false)
+	*/
+}
+
+func NewSimpleClient(subdomain, token string) (*httpsimple.Client, error) {
+	creds := BuildCredentials(subdomain, token)
+	return creds.OAuth2.NewSimpleClient(context.Background())
+	/*
+		return authutil.NewClientHeaderQuery(
+			http.Header{
+				httputilmore.HeaderAuthorization: []string{authutil.TokenBearer + " " + token},
+				AhaAccountHeader:                 []string{subdomain}},
+			url.Values{},
+			false)
+	*/
+}
+
+func BuildCredentials(subdomainOrBaseURL, token string) goauth.Credentials {
+	if !urlutil.IsHTTP(subdomainOrBaseURL, true, true) {
+		subdomainOrBaseURL = fmt.Sprintf(BaseURLFormat, subdomainOrBaseURL)
+	}
+	return goauth.Credentials{
+		Type:    goauth.TypeOAuth2,
+		Service: endpoints.ServiceAha,
+		OAuth2: &goauth.CredentialsOAuth2{
+			ServerURL: subdomainOrBaseURL,
+			Token: &oauth2.Token{
+				AccessToken: token,
+			},
+		},
+	}
 }
 
 /*
