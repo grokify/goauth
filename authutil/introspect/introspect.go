289386ca78148d20fbf34c76535c6cc78600a086
John Wang <johncwang@gmail.com>
2024-07-19 23:26:11 -0700

    feat: `authutil/introspect`: add `IntrospectResponse.Clone()`

--- authutil/introspect/introspect.go
+++ authutil/introspect/introspect.go
@@ -7,11 +7,34 @@ import (
 
 // IntrospectResponse is defined in RFC-7662: https://datatracker.ietf.org/doc/html/rfc7662
 type IntrospectResponse struct {
-	Active   bool   `json:"active"`
-	Scope    string `json:"scope,omitempty"`
-	ClientID string `json:"client_id,omitempty"`
-	Username string `json:"username,omitempty"`
-	Exp      int    `json:"exp,omitempty"`
+	Active    bool   `json:"active"`
+	Aud       string `json:"aud,omitempty"`
+	ClientID  string `json:"client_id,omitempty"`
+	Exp       int    `json:"exp,omitempty"`
+	Iat       int    `json:"iat,omitempty"`
+	Iss       string `json:"iss,omitempty"`
+	Jti       string `json:"jti,omitempty"`
+	Nbf       int    `json:"nbf,omitempty"`
+	Scope     string `json:"scope,omitempty"`
+	Sub       string `json:"sub,omitempty"`
+	TokenType string `json:"token_type,omitempty"`
+	Username  string `json:"username,omitempty"`
+}
+
+func (ir IntrospectResponse) Clone() IntrospectResponse {
+	return IntrospectResponse{
+		Active:    ir.Active,
+		Aud:       ir.Aud,
+		ClientID:  ir.ClientID,
+		Exp:       ir.Exp,
+		Iat:       ir.Iat,
+		Iss:       ir.Iss,
+		Jti:       ir.Jti,
+		Nbf:       ir.Nbf,
+		Scope:     ir.Scope,
+		Sub:       ir.Sub,
+		TokenType: ir.TokenType,
+		Username:  ir.Username}
 }
 
 // MockServer is a mock server that implements RFC-7662 OAuth 2.0 Introspection API endpoint for testing purposes.
@@ -34,13 +57,16 @@ func (ms MockServer) PostIntrospect(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
-	} else if _, ok := ms.ActiveTokens[r.PostFormValue("token")]; ok {
-		ms.Response.Active = true
-	} else {
-		ms.Response.Active = false
 	}
 
-	if body, err := json.Marshal(ms.Response); err != nil {
+	resp := ms.Response.Clone()
+	if _, ok := ms.ActiveTokens[r.PostFormValue("token")]; ok {
+		resp.Active = true
+	} else {
+		resp.Active = false
+	}
+
+	if body, err := json.Marshal(resp); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
 		w.WriteHeader(http.StatusOK)
