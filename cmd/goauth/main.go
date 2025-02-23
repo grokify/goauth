e563cab168814a6d9813ce27bd136f1e3095247e
John Wang <johncwang@gmail.com>
2025-02-23 00:24:39 -0800

    enhance: `cmd/goauth`: update error handling

--- cmd/goauth/main.go
+++ cmd/goauth/main.go
@@ -3,20 +3,27 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/grokify/goauth"
-	"github.com/grokify/mogo/log/logutil"
 	flags "github.com/jessevdk/go-flags"
 )
 
 func main() {
 	cli := goauth.CLIRequest{}
 	_, err := flags.Parse(&cli)
-	logutil.FatalErr(err)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
 
 	err = cli.Do(context.Background(), os.Stdout)
-	logutil.FatalErr(err)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(2)
+	}
 
 	fmt.Println("\nDONE")
+	os.Exit(0)
 }
