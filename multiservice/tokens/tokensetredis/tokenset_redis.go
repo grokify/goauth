0168f76d831201507f5b6885d9d5004727e4a265
John Wang <johncwang@gmail.com>
2025-09-14 10:45:51 -0700

    chore(deps): `go mod`: update

--- multiservice/tokens/tokensetredis/tokenset_redis.go
+++ multiservice/tokens/tokensetredis/tokenset_redis.go
@@ -1,45 +1,46 @@
 package tokensetredis
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/grokify/goauth/multiservice/tokens"
-	"github.com/grokify/sogo/database/document"
-	"github.com/grokify/sogo/database/document/redis"
+	"github.com/grokify/sogo/database/kvs"
+	"github.com/grokify/sogo/database/kvs/redis"
 	"golang.org/x/oauth2"
 )
 
 type TokenSet struct {
-	gsClient document.Client
+	gsClient kvs.Client
 }
 
 func NewTokenSet(client *redis.Client) *TokenSet {
 	return &TokenSet{gsClient: client}
 }
 
-func (toks *TokenSet) GetToken(key string) (*oauth2.Token, error) {
-	if tokInfo, err := toks.GetTokenInfo(key); err != nil {
+func (toks *TokenSet) GetToken(ctx context.Context, key string) (*oauth2.Token, error) {
+	if tokInfo, err := toks.GetTokenInfo(ctx, key); err != nil {
 		return nil, err
 	} else {
 		return tokInfo.Token, nil
 	}
 }
 
-func (toks *TokenSet) GetTokenInfo(key string) (*tokens.TokenInfo, error) {
+func (toks *TokenSet) GetTokenInfo(ctx context.Context, key string) (*tokens.TokenInfo, error) {
 	key = tokens.FormatKey(key)
-	data := toks.gsClient.GetOrEmptyString(key)
+	data := toks.gsClient.GetOrDefaultString(ctx, key, "")
 	if len(strings.TrimSpace(data)) == 0 {
 		return nil, fmt.Errorf("no token for [%v]", key)
 	}
 	return tokens.ParseTokenInfo([]byte(data))
 }
 
-func (toks *TokenSet) SetTokenInfo(key string, tok *tokens.TokenInfo) error {
+func (toks *TokenSet) SetTokenInfo(ctx context.Context, key string, tok *tokens.TokenInfo) error {
 	if bytes, err := json.Marshal(tok); err != nil {
 		return err
 	} else {
-		return toks.gsClient.SetString(tokens.FormatKey(key), string(bytes))
+		return toks.gsClient.SetString(ctx, tokens.FormatKey(key), string(bytes))
 	}
 }
